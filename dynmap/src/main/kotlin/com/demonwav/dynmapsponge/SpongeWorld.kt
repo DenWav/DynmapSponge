ce3944667d0dd5de130ad6638288c3642f5cfe84
DemonWav <demonwav@gmail.com>
2016-09-27 17:08:54 -0500

    Fix constructor in SpongeWorld

--- dynmap/src/main/kotlin/com/demonwav/dynmapsponge/SpongeWorld.kt
+++ dynmap/src/main/kotlin/com/demonwav/dynmapsponge/SpongeWorld.kt
@@ -18,6 +18,7 @@
 
 package com.demonwav.dynmapsponge
 
+import net.minecraft.world.World as NmsWorld
 import org.dynmap.DynmapChunk
 import org.dynmap.DynmapLocation
 import org.dynmap.DynmapWorld
@@ -26,33 +27,53 @@ import org.spongepowered.api.Sponge
 import org.spongepowered.api.block.BlockTypes
 import org.spongepowered.api.data.property.block.LightEmissionProperty
 import org.spongepowered.api.data.property.block.SkyLuminanceProperty
+import org.spongepowered.api.world.DimensionType
 import org.spongepowered.api.world.DimensionTypes
 import org.spongepowered.api.world.World
 
-class SpongeWorld(var world: World) : DynmapWorld(world.name, Sponge.getServer().chunkLayout.spaceSize.y, 63/*todo world.properties.sealevel*/) {
+class SpongeWorld(name: String, height: Int, sealevel: Int, env: DimensionType) : DynmapWorld(name, height, sealevel) {
+
+    private var world: World? = null
+
+    init {
+        when (env) {
+            DimensionTypes.NETHER -> {
+                val f = 0.1F
+                for (i in 0..15) {
+                    val f1 = 1F - i.toFloat() / 15F
+                    // What is even going on here
+                    setBrightnessTableEntry(i, (1F - f1) / (f1 * 3F + 1F) * (1F - f) + f)
+                }
+            }
+        }
+    }
+
+    constructor(world: World) : this(world.name, Sponge.getServer().chunkLayout.spaceSize.y, (world as NmsWorld).seaLevel, world.dimension.type) {
+        setWorldLoaded(world)
+    }
 
     override fun getLightLevel(x: Int, y: Int, z: Int): Int {
-        return world.getLocation(x, y, z).getProperty(LightEmissionProperty::class.java).get?.value ?: 0
+        return world?.getLocation(x, y, z)?.getProperty(LightEmissionProperty::class.java)?.get?.value ?: 0
     }
 
     override fun getSkyLightLevel(x: Int, y: Int, z: Int): Int {
-        return world.getBlock(x, y, z).getProperty(SkyLuminanceProperty::class.java).get?.value?.toInt() ?: 0
+        return world?.getBlock(x, y, z)?.getProperty(SkyLuminanceProperty::class.java)?.get?.value?.toInt() ?: 0
     }
 
     override fun canGetSkyLightLevel(): Boolean {
-        return when (world.dimension.type) {
+        return when (world?.dimension?.type) {
             DimensionTypes.NETHER, DimensionTypes.THE_END -> false
             else -> true
         }
     }
 
     override fun isThundering(): Boolean {
-        return world.properties.isThundering
+        return world?.properties?.isThundering ?: false
     }
 
     override fun getHighestBlockYAt(x: Int, z: Int): Int {
         for (y in Sponge.getServer().chunkLayout.spaceMax.y..Sponge.getServer().chunkLayout.spaceMin.y) {
-            if (world.getBlockType(x, y, z) != BlockTypes.AIR) {
+            if (world?.getBlockType(x, y, z) != BlockTypes.AIR) {
                 return y
             }
         }
@@ -60,15 +81,15 @@ class SpongeWorld(var world: World) : DynmapWorld(world.name, Sponge.getServer()
     }
 
     override fun isLoaded(): Boolean {
-        return world.isLoaded
+        return world?.isLoaded ?: false
     }
 
     override fun isNether(): Boolean {
-        return world.dimension.type == DimensionTypes.NETHER
+        return world?.dimension?.type == DimensionTypes.NETHER
     }
 
     override fun getTime(): Long {
-        return world.properties.worldTime
+        return world?.properties?.worldTime ?: 0
     }
 
     override fun getChunkCache(chunks: MutableList<DynmapChunk>?): MapChunkCache {
@@ -76,7 +97,7 @@ class SpongeWorld(var world: World) : DynmapWorld(world.name, Sponge.getServer()
     }
 
     override fun getEnvironment(): String {
-        return when (world.dimension.type) {
+        return when (world?.dimension?.type) {
             DimensionTypes.NETHER -> "nether"
             DimensionTypes.THE_END -> "the_end"
             // Dynmap can't handle any other type, so just call it normal
@@ -85,12 +106,12 @@ class SpongeWorld(var world: World) : DynmapWorld(world.name, Sponge.getServer()
     }
 
     override fun hasStorm(): Boolean {
-        return world.properties.isRaining
+        return world?.properties?.isRaining ?: false
     }
 
-    override fun getSpawnLocation(): DynmapLocation {
-        val l = world.spawnLocation
-        return DynmapLocation(world.name, l.x, l.y, l.z)
+    override fun getSpawnLocation(): DynmapLocation? {
+        val l = world?.spawnLocation ?: return null
+        return DynmapLocation(world?.name, l.x, l.y, l.z)
     }
 
     fun setWorldLoaded(world: World) {
